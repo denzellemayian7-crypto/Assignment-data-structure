6a3912a6eca733f65260a308270e54025cf62ae8
denzellemayian7-crypto <denzellemayian7@gmail.com>
2026-02-11 15:22:04 +0300

    Update static.py

--- static.py
+++ static.py
@@ -1,1 +1,42 @@
+b. Stack (Linear – LIFO)
+Code : 
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def display(self):
+        print(self.stack)
+
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+s.display()
+s.pop()
+s.display()class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def display(self):
+        print(self.stack)
+
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+s.display()
+s.pop()
+s.display()
 
