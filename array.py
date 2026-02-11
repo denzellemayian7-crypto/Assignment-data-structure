cb089bbef384dc312a101091774a66a578c4a969
denzellemayian7-crypto <denzellemayian7@gmail.com>
2026-02-11 15:17:06 +0300

    Update array.py

--- array.py
+++ array.py
@@ -1,1 +1,47 @@
+a. Array 
+Code : 
+       arr = [10, 20, 30, 40, 50]
+print("Array elements:")
+for item in arr:
+    print(item)
+b. Stack (Linear – LIFO)
+Code : 
+class Stack:
+    def __init__(self):
+        self.stack = []
 
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def display(self):
+        print(self.stack)
+
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+s.display()
+s.pop()
+s.display()class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def display(self):
+        print(self.stack)
+
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+s.display()
+s.pop()
+s.display()
