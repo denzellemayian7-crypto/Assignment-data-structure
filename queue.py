630af1f3ead61094792a7ee9be091b96a23db423
denzellemayian7-crypto <denzellemayian7@gmail.com>
2026-02-11 15:21:19 +0300

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,1 +1,23 @@
+c. Queue (Linear – FIFO)
+Code : 
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        return self.queue.pop(0)
+
+    def display(self):
+        print(self.queue)
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.display()
+q.dequeue()
+q.display()
