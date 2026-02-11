4e8e67b6899b7f36bf56746b533e6c38f584ede7
denzellemayian7-crypto <denzellemayian7@gmail.com>
2026-02-11 15:18:43 +0300

    Update linear.py

--- linear.py
+++ linear.py
@@ -1,1 +1,9 @@
+1.	LINEAR DATA STRUCTURES
+1.1	Array
+Applications:
+                           •  Storing student marks in a system
+                            •  Image pixel storage
 
+                    Reasons:
+•	Allows fast access using index (O(1))
+•	Stores data in contiguous memory
