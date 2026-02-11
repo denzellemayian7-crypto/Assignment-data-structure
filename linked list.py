813a3334b8289cf4c13abfd8b79211db7a3a3e36
denzellemayian7-crypto <denzellemayian7@gmail.com>
2026-02-11 15:19:25 +0300

    Update linked list.py

--- linked list.py
+++ linked list.py
@@ -1,1 +1,10 @@
+1.4 Linked List
+Applications:
+•	Music playlist management
+•	Dynamic memory allocation
+
+
+                    Reasons:
+•	Dynamic size (no fixed memory)
+•	Easy insertion and delete
 
